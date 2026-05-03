dfe4f7b6afa37054b21f623cb36f47f99bd0b6c4
Jarkko Toivonen <jarkko.toivonen@veripalvelu.fi>
2026-05-03 17:46:58 +0300

    Added max_levels parameter to fct_min_count(), and tests

--- R/jmisc.R
+++ R/jmisc.R
@@ -29,38 +29,50 @@ is_whole_number <- function(x, tol = .Machine$double.eps^0.5) {
 #' @param object A factor
 #' @param min Minimum number of occurrences for a level
 #' @param other_level Name of the combination level
+#' @param max_levels Maximum number of levels in the result, including the lumped level
 #'
 #' @returns A factor
 #' @seealso [forcats::fct_lump_min()] lumps levels that appear fewer than min times.
+#' @importFrom forcats fct_other
 #' @export
 #'
 #' @examples
 #' v <- rep(letters[1:4], c(6,5,2,1)) |> as.factor()
 #' v
 #' fct_min_count(v)
-fct_min_count <- function(object, min = 5, other_level = "Other") {
+fct_min_count <- function(object, min = 5, other_level = "Other", max_levels = NA) {
   nas <- is.na(object)
   ll <- levels(object)
   na_count <- sum(nas)
   if (0 < na_count && na_count < min) stop("Total count of NA values is too low")
+  #if (na_count > 0) max_levels <- max_levels - 1
+
   tbl <- table(object)    # Count the elements
   tt <- c(tbl)            # Convert to named vector
   names(tt) <- dimnames(tbl)[[1L]]  # Is this really necessary?
   o <- sort.list(tt, decreasing = TRUE)   # sort.list returns the indices, not the values
+
   # Unite small bins into 'other' bin
-  if (min(tt) >= min) return(object)
+  #if (min(tt) >= min && (is.na(max_levels))) return(object)
+  all_levels_are_large <- min(tt) >= min
+  if (all_levels_are_large && (is.na(max_levels) || length(ll) <= max_levels)) return(object)
 
   #print(o)
   i <- match(TRUE, tt[o] < min)  # Index of the first element that is less than min_count
+  if (is.na(i)) i <- length(ll) + 1
   #print(i)
-  first_to_drop <- i
+
+  # Guarantee that the resulting factor has at most
+  # max_levels levels
+  first_to_drop <- if (is.na(max_levels)) i else min(i, max_levels)
+
   last_to_drop <- match(TRUE, tt[o] == 0) - 1 # Don't drop the non-occurring levels
   if (is.na(last_to_drop)) last_to_drop <- length(ll) # If all levels occur, drop till end
 
   # Nothing to drop
   if (last_to_drop < first_to_drop) return(object)
 
-  #if (! is.na(i) && i < first_to_drop) first_to_drop <- i
+
   if (sum(tt[o[first_to_drop:last_to_drop]]) < min) {
     if (first_to_drop == 1) {
       stop("Total count of non-NA values is too low")
@@ -68,6 +80,7 @@ fct_min_count <- function(object, min = 5, other_level = "Other") {
       first_to_drop <- first_to_drop - 1
     }
   }
+
   drop <- first_to_drop:last_to_drop
   levels_to_unite <- names(tt)[o[drop]]
   #print(levels_to_unite)
--- tests/testthat/test-jmisc.R
+++ tests/testthat/test-jmisc.R
@@ -20,6 +20,10 @@ test_that("doubles with decimals are not whole numbers", {
   expect_false(is_whole_number(1.0001))
 })
 
+####
+#### fct_min_count
+####
+
 test_that("simple_example_with_min_5_works", {
   v <- rep(letters[1:4], c(6,5,2,1)) %>% as.factor()
   res <- fct_min_count(v, min=5)
@@ -59,3 +63,34 @@ test_that("non_occurring_levels_stay_intact2", {
   expect_true(sum(res == "c") == 0)
 })
 
+test_that("small_number_of_nas_is_an_error", {
+  v <- factor(rep(c("a", NA), c(5, 4)), levels=c("a"))
+  expect_error(fct_min_count(v, min=5))
+})
+
+test_that("enough_nas_is_not_a_problem", {
+  v <- factor(rep(c("a", NA), c(5, 5)), levels=c("a"))
+  expect_no_error(fct_min_count(v, min=5))
+})
+
+test_that("the_result_has_at_most_max_levels", {
+  v <- rep(letters[1:4], c(6,5,5,5)) %>% as.factor()
+  res <- fct_min_count(v, min=5, max_levels = 3)
+  expect_all_true(levels(res) == c("a", "b", "Other"))
+  expect_true(sum(res == "a") == 6)
+  expect_true(sum(res == "b") == 5)
+  expect_true(sum(res == "Other") == 10)
+})
+
+test_that("result_is_intact_when_no_lumping_needed", {
+  v <- rep(letters[1:4], c(6,5,5,5)) %>% as.factor()
+  res <- fct_min_count(v, min=5, max_levels = 4)
+  expect_all_true(levels(res) == c("a", "b", "c", "d"))
+  expect_true(sum(res == "a") == 6)
+  expect_true(sum(res == "b") == 5)
+  expect_true(sum(res == "c") == 5)
+  expect_true(sum(res == "d") == 5)
+})
+
+
+# add a test where there are non-occurring levels and max_levels is set
